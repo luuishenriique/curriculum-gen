59d739bd0851564ddfd0262baff917028e07f344
Henrique Martins <henrique_rr_martins@hotmail.com>
2021-05-31 23:14:19 -0300

    Criação do projeto funcional; Adição de valores no mockup; Add pessoa.model.ts, pessoa.component.ts, pessoa.service.ts

--- curriculum-gen-frontend/src/app/app-routing.module.ts
+++ curriculum-gen-frontend/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { RelatorioComponent } from './relatorio/relatorio.component';
+import { RelatorioComponent } from './components/relatorio/relatorio.component';
 
 const routes: Routes = [
   {path: 'relatorio', component: RelatorioComponent}
