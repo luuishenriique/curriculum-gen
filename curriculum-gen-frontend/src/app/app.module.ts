c19fd84a1bba348017cc87bd32bdbdbe7b889666
Henrique Martins <henrique_rr_martins@hotmail.com>
2021-06-20 23:11:00 -0300

    add formulario inicial; add angular/material@11.2.12; add angular/cdk@11.2.12; add angular/animations11.2.12;

--- curriculum-gen-frontend/src/app/app.module.ts
+++ curriculum-gen-frontend/src/app/app.module.ts
@@ -6,7 +6,16 @@ import { AppComponent } from './app.component';
 import { RelatorioComponent } from './components/relatorio/relatorio.component';
 import { PessoaService } from './service/pessoa.service';
 import { ReferenciaComponent } from './components/referencia/referencia.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+  
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,9 +24,18 @@ import { ReferenciaComponent } from './components/referencia/referencia.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
-  providers: [PessoaService],
+  providers: [
+    PessoaService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
