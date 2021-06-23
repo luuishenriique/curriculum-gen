d555c96e96533e683cf92a16fb4c1d1dc691e953
Henrique Martins <henrique_rr_martins@hotmail.com>
2021-06-22 22:46:46 -0300

    Ajustes no formGroup e no html; Inicio do mockup;

--- curriculum-gen-frontend/src/app/app.module.ts
+++ curriculum-gen-frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
 
   
 @NgModule({
@@ -31,7 +32,8 @@ import { MatNativeDateModule } from '@angular/material/core';
     MatIconModule,
     MatFormFieldModule,
     MatDatepickerModule,
-    MatNativeDateModule
+    MatNativeDateModule,
+    MatSelectModule
   ],
   providers: [
     PessoaService
--- curriculum-gen-frontend/src/app/components/relatorio/relatorio.component.ts
+++ curriculum-gen-frontend/src/app/components/relatorio/relatorio.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormControlName, FormGroup, Validators } from '@angular/forms';
 import { PessoaService } from 'src/app/service/pessoa.service';
 
 @Component({
@@ -9,109 +9,130 @@ import { PessoaService } from 'src/app/service/pessoa.service';
 })
 export class RelatorioComponent implements OnInit {
 
-  constructor(private pessoaService: PessoaService, ) { }
+  tipoList: any = [
+    { id: 1, descricao: 'curso' },
+    { id: 2, descricao: 'certificação' },
+    { id: 3, descricao: 'profissionalizante' },
+    { id: 4, descricao: 'técnico' },
+    { id: 5, descricao: 'graduação' },
+    { id: 6, descricao: 'pós-graduação' },
+    { id: 7, descricao: 'mestrado' }
+  ]
+
+  nivelHabilidadeList: any = [
+    { id: 1, nivel: 'iniciante' },
+    { id: 2, nivel: 'intermediário' },
+    { id: 3, nivel: 'avançado' },
+  ]
+
+  constructor(
+    private pessoaService: PessoaService,
+    private formBuilder: FormBuilder,
+    ) { }
 
   pessoa = {
-    id: 0,
-    nome: '',
-    dtNasc: new Date('01/01/1990'),
-    email: '',
-    ddd: '',
-    celular: '',
-    telefone: '',
+    id: 1,
+    nome: 'Henrique Martins',
+    dtNasc: new Date('07/09/1989'),
+    email: 'henrique_rr_martins@hotmail.com',
+    ddd: '81',
+    celular: 'x xxxx-xxxx',
+    telefone: 'xxxx-xxxx',
     endereco: {
-      id: 0,
-      cep: '',
-      uf: '',
-      cidade: '',
-      bairro: '',
-      rua: '',
-      numero: ''
+      id: 1,
+      cep: '54783-010',
+      uf: 'PE',
+      cidade: 'Recife',
+      bairro: 'Aldeia dos Camarás',
+      rua: 'Est. Aldeia',
+      numero: '11971'
     },
-    redeSocial:{
-      id: 0,
-      nome: '',
-      link: ''
+    redeSocial: {
+      id: 1,
+      nome: 'Linkedin',
+      link: 'https://www.linkedin.com/in/henrique-martins-450643119/'
     },
-    historicoProfissional:{
-      id: 0,
-      empresa: '',
-      cargo: '',
-      dtInicio: '',
-      dtFim: '',
-      referencia:{
-        id: 0,
-        nome: '',
-        email: '',
-        ddd: '',
-        telefone: ''
+    historicoProfissional: {
+      id: 1,
+      empresa: 'Tascom',
+      cargo: 'Desenvolvedor',
+      dtInicio: new Date('11/23/2020'),
+      dtFim: new Date('05/17/2021'),
+      referencia: {
+        id: 1,
+        nome: 'Felipe',
+        email: 'f@tascom.com.br',
+        ddd: '81',
+        celular: 'x xxxx-xxxx'
       }
     },
     educacao: {
-      id: 0,
-      instituicao: '',
-      curso: '',
-      dtInicio: new Date('01/01/1990'),
-      dtFim: new Date('01/01/1990'),
+      id: 1,
+      instituicao: 'UNIS-MG',
+      curso: 'Analise e desenvolvimento de sistemas',
+      dtInicio: new Date('02/02/2015'),
+      dtFim: new Date('08/10/2018'),
       tipo: {
-        id: 0,
-        descricao: ''
+        id: 5,
+        descricao: 'Graduação'
       }
     },
     habilidade: {
-      id: 0,
-      descricao: '',
-      nivelHabilidade:{
-        id: 0,
-        nivel: ''
+      id: 1,
+      descricao: 'JavaScript',
+      nivelHabilidade: {
+        id: 2,
+        nivel: 'Intermediário'
       }
     }
   }
 
-  public curriculumForm: FormGroup = new FormGroup({
-    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
-    dtNasc: new FormControl(),
-    email: new FormControl(),
-    ddd: new FormControl(),
-    celular: new FormControl(),
-    telefone: new FormControl(),
-    endereco: new FormGroup({
-      cep: new FormControl(),
-      uf: new FormControl(),
-      cidade: new FormControl(),
-      bairro: new FormControl(),
-      rua: new FormControl(),
-      numero: new FormControl()
+  public curriculumForm: FormGroup = this.formBuilder.group({
+    nome: this.pessoa.nome,
+    dtNasc: this.pessoa.dtNasc,
+    email: this.pessoa.email,
+    ddd: this.pessoa.ddd,
+    celular: this.pessoa.celular,
+    telefone: this.pessoa.telefone,
+    endereco: this.formBuilder.group({
+      cep: this.pessoa.endereco.cep,
+      uf: this.pessoa.endereco.uf,
+      cidade: this.pessoa.endereco.cidade,
+      bairro: this.pessoa.endereco.bairro,
+      rua: this.pessoa.endereco.rua,
+      numero: this.pessoa.endereco.numero
     }),
-    redeSocial: new FormGroup({
-      nome: new FormControl(),
-      link: new FormControl()
+    redeSocial: this.formBuilder.group({
+      nome: this.pessoa.redeSocial.nome,
+      link: this.pessoa.redeSocial.link
     }),
-    historicoProfissional: new FormGroup({
-      empresa: new FormControl(),
-      cargo: new FormControl(),
-      dtInicio: new FormControl(),
-      dtFim: new FormControl(),
+    historicoProfissional: this.formBuilder.group({
+      empresa: this.pessoa.historicoProfissional.empresa,
+      cargo: this.pessoa.historicoProfissional.cargo,
+      dtInicio: this.pessoa.historicoProfissional.dtInicio,
+      dtFim: this.pessoa.historicoProfissional.dtFim,
 
-      referencia: new FormGroup({
-        nome: new FormControl(),
-        email: new FormControl(),
-        telefone: new FormControl()
+      referencia: this.formBuilder.group({
+        nome: this.pessoa.historicoProfissional.referencia.nome,
+        email: this.pessoa.historicoProfissional.referencia.email,
+        celular: this.pessoa.historicoProfissional.referencia.celular
       })
     }),
-    educacao: new FormGroup({
-      instituicao: new FormControl(),
-      curso: new FormControl(),
-      dtInicio: new FormControl(),
-      dtFim: new FormControl(),
-      tipo: new FormGroup({
-        nivel: new FormControl()
+    educacao: this.formBuilder.group({
+      instituicao: this.pessoa.educacao.instituicao,
+      curso: this.pessoa.educacao.curso,
+      dtInicio: this.pessoa.educacao.dtInicio,
+      dtFim: this.pessoa.educacao.dtFim,
+      tipo: this.formBuilder.group({
+        id: this.pessoa.educacao.tipo.id,
+        descricao: this.pessoa.educacao.tipo.descricao
       })
     }),
-    habilidade: new FormGroup({
+    habilidade: this.formBuilder.group({
       descricao: new FormControl(),
-      nivelHabilidade: new FormGroup({
-        nivel: new FormControl()
+      nivelHabilidade: this.formBuilder.group({
+        id: this.pessoa.habilidade.nivelHabilidade.id,
+        nivel: this.pessoa.habilidade.nivelHabilidade.nivel
       })
     })
   });
@@ -120,7 +141,7 @@ export class RelatorioComponent implements OnInit {
     console.log(this.curriculumForm.getRawValue());
   }
 
-  teste(teste: any): void{
+  teste(teste: any): void {
     console.log(teste);
   }
 }
