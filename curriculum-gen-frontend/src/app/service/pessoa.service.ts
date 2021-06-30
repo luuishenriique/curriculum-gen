23191969a2874341d33d175e6a598d88071e4c62
Henrique Martins <henrique_rr_martins@hotmail.com>
2021-06-30 00:20:56 -0300

    ajuste classe PessoaService: Feito mockup para testes;

--- curriculum-gen-frontend/src/app/service/pessoa.service.ts
+++ curriculum-gen-frontend/src/app/service/pessoa.service.ts
@@ -1,4 +1,5 @@
 import { Pessoa } from "../model/pessoa.model";
+import { Endereco } from "../model/endereco.model";
 
 export class PessoaService{
 
@@ -15,16 +16,159 @@ export class PessoaService{
         //Seguir o padrão
         let pessoa: Pessoa;
         //Variável setada de forma manual
-        pessoa = { "id": 1, "nome": "Alejandro", "dtNasc": new Date("09/07/1989"), "email": "alejandro_futebol@hotmail.com", "ddd": "(xx)", "celular": "x xxxx-xxxx", "telefone": "xxxx-xxxx" };
+        pessoa = { 
+            "id": 1, 
+            "nome": "Alejandro",
+            "dtNasc": new Date("09/07/1989"), 
+            "email": "alejandro_futebol@hotmail.com", 
+            "celular": "x xxxx-xxxx", 
+            "telefone": "xxxx-xxxx" ,
+            "endereco":{
+                "id": 1,
+                "cep": "54783-010",
+                "uf": "PE",
+                "cidade": "Recife",
+                "bairro": "Bairro Novo",
+                "rua": "Rua Teste",
+                "numero": "128"
+            },
+            redesSociais:[
+                { 
+                    "id": 1,
+                    "nome": "Henrique Martins",
+                    "link": "facebook.com/henrique"
+                }
+            ],
+            empregos:[
+                {
+                    "id": 1,
+                    "empresa": "Flexssonia",
+                    "cargo": "Dev I",
+                    "dtInicio": new Date('17/05/2021'),
+                    "dtFim": new Date('01/01/1990'),
+                    "referencia":{
+                        "id": 1,
+                        "nome": "Túlio",
+                        "email": "tulio@gmail.com",
+                        "celular": "x xxxx-xxxx"
+                    }
+                }
+            ],
+            cursos: [
+                {
+                    "id": 1,
+                    "instituicao": "UNIS-MG",
+                    "curso": "ADS",
+                    "dtInicio": new Date('02/02/2015'),
+                    "dtFim": new Date('10/08/2017'),
+                    "tipo":{
+                        "id": 4,
+                        "descricao": "graduação"
+                    }
+                }
+            ],
+            habilidades:[
+                { 
+                    "id": 1,
+                    "descricao": "Java",
+                    "nivelHabilidade":{
+                        "id": 0, 
+                        "nivel": "iniciante"
+                    }
+                },
+                { 
+                    "id": 2,
+                    "descricao": "Angular",
+                    "nivelHabilidade":{
+                        "id": 0, 
+                        "nivel": "intermediário"
+                    }
+                }
+            ]
+        };
 
         //Através do comando push adicionamos um objeto a lista.
         pessoaList.push(pessoa)
 
         //Adição de mais um objeto a lista
-        pessoa = { "id": 2, "nome": "André", "dtNasc": new Date("09/07/1989"), "email": "andre_d@hotmail.com", "ddd": "(xx)", "celular": "x xxxx-xxxx", "telefone": "xxxx-xxxx" };
+        pessoa = { "id": 2, "nome": "André", "dtNasc": new Date("09/07/1989"), "email": "andre_d@hotmail.com", "celular": "x xxxx-xxxx", "telefone": "xxxx-xxxx" };
         pessoaList.push(pessoa)
 
         //Retorna a lista de pessoas.
         return pessoaList;
+    }
+
+    public getPessoaById(): Pessoa{
+        let pessoa: Pessoa = { 
+            "id": 1, 
+            "nome": "Alejandro",
+            "dtNasc": new Date("09/07/1989"), 
+            "email": "alejandro_futebol@hotmail.com", 
+            "celular": "x xxxx-xxxx", 
+            "telefone": "xxxx-xxxx" ,
+            "endereco":{
+                "id": 1,
+                "cep": "54783-010",
+                "uf": "PE",
+                "cidade": "Recife",
+                "bairro": "Bairro Novo",
+                "rua": "Rua Teste",
+                "numero": "128"
+            },
+            redesSociais:[
+                { 
+                    "id": 1,
+                    "nome": "Henrique Martins",
+                    "link": "facebook.com/henrique"
+                }
+            ],
+            empregos:[
+                {
+                    "id": 1,
+                    "empresa": "Flexssonia",
+                    "cargo": "Dev I",
+                    "dtInicio": new Date('17/05/2021'),
+                    "dtFim": new Date('01/01/1990'),
+                    "referencia":{
+                        "id": 1,
+                        "nome": "Túlio",
+                        "email": "tulio@gmail.com",
+                        "celular": "x xxxx-xxxx"
+                    }
+                }
+            ],
+            cursos: [
+                {
+                    "id": 1,
+                    "instituicao": "UNIS-MG",
+                    "curso": "ADS",
+                    "dtInicio": new Date('02/02/2015'),
+                    "dtFim": new Date('10/08/2017'),
+                    "tipo":{
+                        "id": 4,
+                        "descricao": "graduação"
+                    }
+                }
+            ],
+            habilidades:[
+                { 
+                    "id": 1,
+                    "descricao": "Java",
+                    "nivelHabilidade":{
+                        "id": 0, 
+                        "nivel": "iniciante"
+                    }
+                },
+                { 
+                    "id": 2,
+                    "descricao": "Angular",
+                    "nivelHabilidade":{
+                        "id": 0, 
+                        "nivel": "intermediário"
+                    }
+                }
+            ]
+        };
+        return pessoa;
     }
 }
